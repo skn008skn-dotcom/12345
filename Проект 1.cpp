890807ca87882c6b7e0baf088dceb2d875078e18
skn008skn-dotcom <skn008skn@gmail.com>
2025-10-13 19:03:49 +0300

    Add files via upload

---
+++ Проект 1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <iomanip>  
+
+int main() {
+    const int size = 9;
+
+    for (int i = 1; i <= size; ++i) {
+        for (int j = 1; j <= size; ++j) {
+            std::cout << std::setw(4) << i * j; 
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
