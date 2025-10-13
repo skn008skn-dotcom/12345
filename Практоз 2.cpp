bfe78b5951e091fb7915366b2a312d8abd853102
skn008skn-dotcom <skn008skn@gmail.com>
2025-10-13 19:17:04 +0300

    Add files via upload

---
+++ Практоз 2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <clocale>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");     
+    SetConsoleCP(1251);                    
+    SetConsoleOutputCP(1251);               
+
+    int n;
+    cout << "Введите число: ";
+    cin >> n;
+
+    cout << "Делители числа " << n << ": ";
+    for (int i = 1; i <= n; ++i) {
+        if (n % i == 0) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
