e36004925b7d2e2ff31da339f29bd2731671333e
skn008skn-dotcom <skn008skn@gmail.com>
2025-10-20 16:57:03 +0300

    Add files via upload

---
+++ практическая 3, задание 1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    int n;
+    cout << "Введите трёхзначное число: ";
+    cin >> n;
+
+    
+    if (n < 100 || n > 999) {
+        cout << "Ошибка! Введите трёхзначное число." << endl;
+        return 1;
+    }
+
+    int last_digit = n % 10;       
+    int first_two_digits = n / 10;  
+
+    int new_number = last_digit * 100 + first_two_digits;
+
+    cout << "Полученное число: " << new_number << endl;
+
+    return 0;
+}
